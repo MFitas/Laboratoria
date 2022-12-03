091ee51e7ce1e20defd21ce5976ec2f4e453fd1d
MFitas <mateuszfitas1990@gmail.com>
2022-12-03 12:34:12 +0100

    Lab 5 z2 FINISHED

---
+++ Lab05MF/src/Building.java
@@ -0,0 +1,52 @@
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Building
+{
+    String Nazwa;
+    LocalDate RokBudowy;
+    int LiczbaPieter;
+
+    public Building(String nazwa, LocalDate rokBudowy, int licczbaPieter) {
+        Nazwa = nazwa;
+        RokBudowy = rokBudowy;
+        LiczbaPieter = licczbaPieter;
+    }
+
+    public String getNazwa() {
+        return Nazwa;
+    }
+
+    public LocalDate getRokBudowy() {
+        return RokBudowy;
+    }
+
+    public int getLiczbaPieter() {
+        return LiczbaPieter;
+    }
+
+    public void setNazwa(String nazwa) {
+        Nazwa = nazwa;
+    }
+
+    public void setRokBudowy(LocalDate rokBudowy) {
+        RokBudowy = rokBudowy;
+    }
+
+    public void setLiczbaPieter(int liczbaPieter) {
+        LiczbaPieter = liczbaPieter;
+    }
+
+    public void piszDane(){
+        System.out.println("Nazwa budynku: "+this.Nazwa);
+        System.out.println("Liczba pieter: "+this.LiczbaPieter);
+        System.out.println("Rok budowy: "+this.RokBudowy);
+    }
+
+    public Period wiekBudynku(){
+        LocalDate aktualnyRok = LocalDate.now();
+        Period diff = Period.between(this.RokBudowy, aktualnyRok);
+        return diff;
+    }
+}
--- Lab05MF/src/Main.java
+++ Lab05MF/src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args)
     {
@@ -6,5 +8,15 @@ public class Main {
                 " and " + p1.getLength() + " has Area equal to: " +p1.getArea()+
                 " has Circumference equal to "+p1.getCircumference()+
                 " and has diagonal equal to: "+ p1.getdiagonal());
+
+        Building bud1 = new Building("domek", LocalDate.of(1990,10,15),2);
+        Building bud2 = new Building("mały domek", LocalDate.of(1950,10,15),2);
+        Building bud3 = new Building("duży domek", LocalDate.of(1999,10,15),2);
+
+        System.out.println("Wiek budynku: "+bud1.wiekBudynku().getYears());
+        bud1.piszDane();
+        bud2.piszDane();
+        bud3.piszDane();
+
     }
 }
